4141ac2f839f81c72e33359fc8bb12d3406b8acc
liamVScode <doducliem07052002@gmail.com>
2023-10-19 14:33:13 +0700

    `feat`: make model from table

--- app/Models/ShippingAddress.php
+++ app/Models/ShippingAddress.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class ShippingAddress extends Model
 {
     use HasFactory;
 }

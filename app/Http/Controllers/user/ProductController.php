ae16cbd1fa6a68d60f196e144c31d26089cd2e97
Quỳnh Lê <lxq.2309@gmail.com>
2023-11-13 07:31:52 +0700

    fix bug

--- app/Http/Controllers/user/ProductController.php
+++ app/Http/Controllers/user/ProductController.php
@@ -35,6 +35,10 @@ class ProductController extends Controller
             ->select('Genre.*')
             ->get();
         $avgRating = DB::table('Review')->where('BookID', $id)->avg('Rating');
+        if ($avgRating == null)
+        {
+            $avgRating = 0;
+        }
         $rounderIntRating = intval(round($avgRating));
         $checkSale = DB::table('User')
             ->join('SalesOrder', 'User.UserID', "=", 'SalesOrder.UserID')
